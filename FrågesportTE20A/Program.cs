c004c44a7071963df09cdd9cde6332188a11ce26
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2021-10-06 13:26:05 +0200

    hej

--- FrågesportTE20A/Program.cs
+++ FrågesportTE20A/Program.cs
@@ -2,9 +2,59 @@
 
 string name = "Johan"; 
 
-int a = 18; 
+Console.WriteLine("Hej välkommen till Johan Emsgård's Frågesport");
 
-Console.WriteLine("Hej välkommen till Johan Emsgård's Frågesport")
-Console.ReadLine() 
+int points = 1; 
 
 
+Console.WriteLine($"Points: {points}");
+Console.WriteLine("Vad heter Nitendos Mascott?");  
+string answer = Console.ReadLine(); 
+
+if (answer == "Mario")
+{
+    Console.WriteLine("Correct"); 
+    points++;
+}
+
+else 
+{
+    Console.WriteLine("Wrong"); 
+}
+
+Console.WriteLine($"points: {points}");
+Console.WriteLine("What does CPU stand for?"); 
+answer = Console.ReadLine(); 
+
+if (answer == "Central Proccessing Unite") 
+{
+    Console.WriteLine("Correct");
+    points++; 
+}
+
+else 
+{
+    Console.WriteLine("Wrong");
+}
+
+Console.WriteLine($"points: {points}");
+Console.WriteLine("When did the first computer come out?");
+Console.WriteLine("a) 1943");
+Console.WriteLine("b) 1999");
+Console.WriteLine("c) 1869");
+answer = Console.ReadLine(); 
+
+if ( answer == "1943")
+{
+    Console.WriteLine("Correct"); 
+    points++; 
+}
+
+else 
+{
+    Console.WriteLine("Wrong");
+}
+
+
+
+Console.ReadLine(); 
